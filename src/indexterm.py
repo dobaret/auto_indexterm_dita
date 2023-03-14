82fee7aa0d3b066a08b2d62a1610a56f244144b8
Dorian Baret <91059151+dobaret@users.noreply.github.com>
2023-03-14 08:48:26 +0100

    Multiple terms as input (with commas)

--- src/indexterm.py
+++ src/indexterm.py
@@ -25,85 +25,91 @@ def search_for_string():
         return
 
     # Convert the search string to lowercase to match no matter the case
-    search_string = search_string.lower()
+    search_terms = [term for term in search_string.split(",")]
 
-    # Get a list of all DITA files in the folder
-    file_list = [f for f in os.listdir(folder_path) if f.endswith(".dita")]
+    # Loop through each search term
+    for search_term in search_terms:
 
-    # Initialize a counter for the number of changes
-    change_count = 0
+        # Convert the search term to lowercase to match no matter the case
+        search_term = search_term.lower()
 
-    # Loop through the files in the folder
-    for file_name in file_list:
+        # Get a list of all DITA files in the folder
+        file_list = [f for f in os.listdir(folder_path) if f.endswith(".dita")]
 
-        # Open the file in read mode
-        with open(os.path.join(folder_path, file_name), "r", encoding="utf-8") as f:
+        # Initialize a counter for the number of changes for this search term
+        change_count = 0
 
-            # Read the entire file as a string
-            file_contents = f.read()
-            
-        # Check if the search string is in the file
-        if search_string in file_contents.lower():
+        # Loop through the files in the folder
+        for file_name in file_list:
 
-            # The search string was found, so check if there are any existing indexterm tags
-            existing_indexterms = [term.lower() for term in re.findall(r"<indexterm>([^<]*)</indexterm>", file_contents)]
+            # Open the file in read mode
+            with open(os.path.join(folder_path, file_name), "r", encoding="utf-8") as f:
+
+                # Read the entire file as a string
+                file_contents = f.read()
                 
-            # If the search string is not in the list of existing indexterms, then,
-            if search_string not in existing_indexterms:
+            # Check if the search string is in the file
+            if search_term in file_contents.lower():
 
-                # If there are existing indexterm tags, add the search string after the last one
-                if len(existing_indexterms) > 0:
-
-                    # Get the index of the last indexterm tag
-                    last_index = file_contents.find("</indexterm>")
-
-                    # Add the length of the closing indexterm tag to get the index immediately after it
-                    last_index += len("</indexterm>")
-
-                    # Insert the new indexterm after the last indexterm tag
-                    if acronym_var.get():
-                        new_contents = file_contents[:last_index] + "\n                <indexterm>" + search_string.upper() + "</indexterm>" + file_contents[last_index:]
-                    else:
-                        new_contents = file_contents[:last_index] + "\n                <indexterm>" + search_string.capitalize() + "</indexterm>" + file_contents[last_index:]
-
-                # If there are no existing indexterm tags, add the search string after the title or shortdesc tag
-                else:
-                    # Check if the shortdesc tag is present in the file
-                    if "<critdates>" in file_contents:
-                        new_contents = re.sub(
-                            r"<critdates>(.*)(?<!<)</critdates>",
-                            r"<critdates>\1</critdates>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_string.upper() if acronym_var.get() else search_string.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>",
-                            file_contents,
-                            flags=re.DOTALL
-                        )
-                    elif "<shortdesc>" in file_contents:
-                        # Use the shortdesc tag instead of the title tag
-                        new_contents = re.sub(
-                            r"<shortdesc>(.*)(?<!<)</shortdesc>",
-                            r"<shortdesc>\1</shortdesc>\n    <prolog>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_string.upper() if acronym_var.get() else search_string.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>\n    </prolog>",
-                            file_contents,
-                            flags=re.DOTALL
-                        )
-                    else:
-                        new_contents = re.sub(
-                            r"<title>(.*)(?<!<)</title>",
-                            r"<title>\1</title>\n    <prolog>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_string.upper() if acronym_var.get() else search_string.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>\n    </prolog>",
-                            file_contents,
-                            count=1
-                        )
-
-                # Open the file in write mode
-                with open(os.path.join(folder_path, file_name), "w", encoding="utf-8") as f:
+                # The search string was found, so check if there are any existing indexterm tags
+                existing_indexterms = [term.lower() for term in re.findall(r"<indexterm>([^<]*)</indexterm>", file_contents)]
                     
-                    # Write the updated file contents to the file
-                    f.write(new_contents)
+                # If the search string is not in the list of existing indexterms, then,
+                if search_term not in existing_indexterms:
 
-                    # Increment the change counter
-                    change_count += 1
+                    # If there are existing indexterm tags, add the search string after the last one
+                    if len(existing_indexterms) > 0:
+
+                        # Get the index of the last indexterm tag
+                        last_index = file_contents.find("</indexterm>")
+
+                        # Add the length of the closing indexterm tag to get the index immediately after it
+                        last_index += len("</indexterm>")
+
+                        # Insert the new indexterm after the last indexterm tag
+                        if acronym_var.get():
+                            new_contents = file_contents[:last_index] + "\n                <indexterm>" + search_term.upper() + "</indexterm>" + file_contents[last_index:]
+                        else:
+                            new_contents = file_contents[:last_index] + "\n                <indexterm>" + search_term.capitalize() + "</indexterm>" + file_contents[last_index:]
+
+                    # If there are no existing indexterm tags, add the search string after the title or shortdesc tag
+                    else:
+                        # Check if the shortdesc tag is present in the file
+                        if "<critdates>" in file_contents:
+                            new_contents = re.sub(
+                                r"<critdates>(.*)(?<!<)</critdates>",
+                                r"<critdates>\1</critdates>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_term.upper() if acronym_var.get() else search_term.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>",
+                                file_contents,
+                                flags=re.DOTALL
+                            )
+                        elif "<shortdesc>" in file_contents:
+                            # Use the shortdesc tag instead of the title tag
+                            new_contents = re.sub(
+                                r"<shortdesc>(.*)(?<!<)</shortdesc>",
+                                r"<shortdesc>\1</shortdesc>\n    <prolog>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_term.upper() if acronym_var.get() else search_term.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>\n    </prolog>",
+                                file_contents,
+                                flags=re.DOTALL
+                            )
+                        else:
+                            new_contents = re.sub(
+                                r"<title>(.*)(?<!<)</title>",
+                                r"<title>\1</title>\n    <prolog>\n        <metadata>\n            <keywords>\n                <indexterm>" + (search_term.upper() if acronym_var.get() else search_term.capitalize()) + "</indexterm>\n            </keywords>\n        </metadata>\n    </prolog>",
+                                file_contents,
+                                count=1
+                            )
+
+                    # Open the file in write mode
+                    with open(os.path.join(folder_path, file_name), "w", encoding="utf-8") as f:
+                        
+                        # Write the updated file contents to the file
+                        f.write(new_contents)
+
+                        # Increment the change counter
+                        change_count += 1
 
         # Example search result
-        result_label.config(text="Résultat de la recherche pour '{}' : {} fichiers trouvés.".format(search_string, change_count))
-
+        result_label.config(text="Résultat de la recherche pour '{}' : {} fichiers trouvés.".format(search_term, change_count))
+            
 # Define a function to browse for the folder path
 def browse_folder_path():
     global folder_path
